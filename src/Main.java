7f38e8228455421b01bee1c293533352e4ee6cec
Théobinch <theo.lepilliet@edu.esiee-it.fr>
2025-01-22 13:23:16 +0100

    add secretsound

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,7 @@
 import java.util.Scanner;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
 
 public class Main {
 
@@ -13,8 +16,17 @@ public class Main {
 
     public static void main(String[] args) {
         menu();
+        secretSound();
     }
 
+
+    //Fonction menu
+    //Ajout d'un scanner pour prendre en compte la touche saisit par l'user
+    //Print 'Choisit une option', choix 1, 2, 3, 4, 5
+    //boucle do while qui s'arrete lorsque l'user selectionne une reponse valide
+    //verifie si l'element entré par l'user est un int
+    //si 1 tapé print Play, si 2 tapé print rules, etc...
+    //sinon print 'entrez une nombre valide'
     /**
      * Displays the menu of the corresponding input
      * @return
@@ -26,8 +38,8 @@ public class Main {
         Scanner input = new Scanner(System.in);
 
         do { //do while loop that stops when the user enters a valid element
-            System.out.println(YELLOW + "Select an option");
-            System.out.println(GREEN + "*****************");
+            System.out.println(YELLOW + "Select an option" + RESET);
+            System.out.println(GREEN + "*****************" + RESET);
             System.out.println(CYAN + "1 / Play");
             System.out.println("2 / Rules");
             System.out.println("3 / Load");
@@ -36,7 +48,6 @@ public class Main {
 
             if (input.hasNextInt()) { //if the field entered by the user is an int, the selection takes the int into account
                 selection = input.nextInt();
-
                 if (selection == 1) { //if int is 1, print “Play”
                     System.out.println("Play");
                     return selection;
@@ -57,36 +68,69 @@ public class Main {
                     return selection;
                 }
                 if(selection < 1 || selection > 5){
-                    System.out.println(RED + "Enter a valid number !");
+                    System.out.println(RED + "Enter a valid number !" + RESET);
                 }
             }
-            else{ //otherwise a valid element is requested.
-                System.out.println(RED + "Please enter a valid item !");
+            else{//otherwise a valid element is requested.
+                System.out.println(RED + "Please enter a valid item !" + RESET);
                 input.next();
             }
         }while (true);
     }
 
+
+    //Fonction rules
+    //print les regles, etc...
+    //print 'E pour sortir'
+    //boucle do while s'arrete que si l'user entre element valide
+    //si touche e / E entré, retourne au menu
+     //si autre touche entré, print 'entrer une lettre corret'
     /**
      * Displays game rules
      */
     public static void rules(){
 
         System.out.println(GREEN + "\n**** Welcome to This is a game **** \n" +
-                        PURPLE + "\nGoal : The goal is to be the last player able to move on the board.\n" +
-                        "\nMove : Move your pawn one square, either vertically or horizontally." +
-                        "\nDestroy : an adjacent square (not occupied by a player).\n" +
-                        "\nConstraints :\n" + "You cannot destroy a square occupied by a player.\n"
-                        + "You cannot move onto a destroyed or already occupied square.\n" + "If you get stuck, you lose the game.\n" +
-                        "\nPoints :\n" + "5 points for each victory.\n" + "-2 points for each defeat.\n");
+                PURPLE + "\nGoal : The goal is to be the last player able to move on the board.\n" +
+                "\nMove : Move your pawn one square, either vertically or horizontally." +
+                "\nDestroy : an adjacent square (not occupied by a player).\n" +
+                "\nConstraints :\n" + "You cannot destroy a square occupied by a player.\n"
+                + "You cannot move onto a destroyed or already occupied square.\n" + "If you get stuck, you lose the game.\n" +
+                "\nPoints :\n" + "5 points for each victory.\n" + "-2 points for each defeat.\n");
 
+        returnLoop();
+    }
+
+    public static void returnLoop(){
         System.out.println(YELLOW + "Press (E) to exit !");
         do{ //do while loop that stops when e is pressed
             Scanner input = new Scanner(System.in);
             if (input.hasNext("e") || input.hasNext("E")) //press e/E to exit the rules
                 return;
-            if (input.hasNext())
+            else if (input.hasNext())
                 System.out.println(RED + "Enter the correct letter !");
         }while (true);
     }
+
+    /**
+     * Displays secretSound
+     */
+    public static void secretSound() {
+        new Thread(new Runnable() {
+
+            public void run() {
+                try {
+                    Clip clip = AudioSystem.getClip();
+                    AudioInputStream inputStream = AudioSystem.getAudioInputStream(
+                            Main.class.getResourceAsStream("you-are-my-sunshine-made-with-Voicemod.wav"));
+                    clip.open(inputStream);
+                    clip.start();
+
+                } catch (Exception e) {
+                    System.err.println(e.getMessage());
+                }
+            }
+        }).start();
+    }
 }
+
